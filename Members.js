472ad90b7ba9e0c012bb56ebecd70f0c3d7b923e
vennelakoilada362 <vennelakoilada@gmail.com>
2020-07-08 00:39:14 +0530

    first commit

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        'id' : 1,
+        'name' : 'Vennela',
+        'email' :'venneskd@gamil.com',
+        'status' : 'active'
+    },
+    {
+        'id' : 2,
+        'name' : 'Rashi',
+        'email' :'rashi@gamil.com',
+        'status' : 'inactive'
+    },
+    {
+        'id' : 3,
+        'name' : 'Sabita',
+        'email' : 'vejhgd@gamil.com',
+        'status' : 'active'
+    }
+    ];
+
+    module.exports = members;
---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const logger = require('./middleware/logger');
+const members =require('./Members');
+
+const app = express();
+
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.get('/', (req, res) => res.render('index', {
+    title:'Member app',
+    members
+})
+);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/members', require('./routes/api/members'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
